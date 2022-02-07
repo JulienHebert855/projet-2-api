254554fff2e84790bf4a34b9a8792ae5144f40af
Julien Hébert <73999978+JulienHebert855@users.noreply.github.com>
2022-02-06 23:37:48 -0500

    Add files via upload

---
+++ include/fonctions.php
@@ -0,0 +1,43 @@
+<?php
+
+// Cette fonction prend l'object au format tablulaire SQL 
+// et retourne un objet dont la structure correspond au format
+// devant être retourné par l'API. 
+function ConversionForfaitSQLEnObjet($forfaitSQL) {
+    $forfaitOBJ = new stdClass();
+    $forfaitOBJ->destination = $forfaitSQL["destination"];
+
+    $forfaitOBJ->villeDepart = $forfaitSQL["villeDepart"];
+
+    $forfaitOBJ->hotel = new stdClass();
+    $forfaitOBJ->hotel->nom = $forfaitSQL["nom"];
+    $forfaitOBJ->hotel->coordonnees = $forfaitSQL["coordonnees"];
+    $forfaitOBJ->hotel->nombreEtoiles = $forfaitSQL["nombreEtoiles"];
+    $forfaitOBJ->hotel->nombreChambres = $forfaitSQL["nombreChambres"];
+    $forfaitOBJ->hotel->caracteristiques = explode (";", $forfaitSQL["caracteristiques"]);
+
+    $forfaitOBJ->dateDepart = $forfaitSQL["dateDepart"];
+
+    $forfaitOBJ->dateRetour = $forfaitSQL["dateRetour"];
+
+    $forfaitOBJ->prix = $forfaitSQL["prix"];
+
+    $forfaitOBJ->rabais = $forfaitSQL["rabais"];
+
+    $forfaitOBJ->vedette = $forfaitSQL["vedette"];
+
+    return $forfaitOBJ;
+
+}   
+
+function ConversionCommentaireSQLEnObjet($commentaireSQL) {
+    $commentaireOBJ = new stdClass();
+    $commentaireOBJ->nomClient = $commentaireSQL["nomClient"];
+    $commentaireOBJ->dateCommentaire = $commentaireSQL["dateCommentaire"];
+    $commentaireOBJ->commentaire = $commentaireSQL["commentaire"];
+
+    return $commentaireOBJ;
+
+}  
+
+?>
